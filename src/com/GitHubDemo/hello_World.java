02eb865c9bf8de59a1ff9255690b6c2d230e7c38
Priyanka yadav <piyuyadav528@gmail.com>
2021-12-28 13:21:49 +0530

    first commit

---
+++ src/com/GitHubDemo/hello_World.java
@@ -0,0 +1,11 @@
+package com.GitHubDemo;
+
+public class hello_World {
+	public static void name() 
+	{
+		System.out.println("Good Afternoon");
+		System.out.println("Welcome To Github");
+	}
+	 
+
+}
